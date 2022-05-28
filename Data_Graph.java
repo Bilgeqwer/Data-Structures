88934abe76b9cb37a2b53e8eb99832e8a1abb993
Bilgekul <93567431+Bilgeqwer@users.noreply.github.com>
2022-05-28 17:07:40 +0300

    Update Data_Graph.java

--- Data_Graph.java
+++ Data_Graph.java
@@ -1,18 +1,58 @@
+
+
 public class Data_Graph{
     public static void main(String[] args) {
        Graph g = new Graph(5); // Graph is created with 5 nodes
        int a[][] = { {0,1,0,1,1}, {1,0,1,1,0}, {0,1,0,1,1}, {1,1,1,0,0}, 
        {1,0,1,0,0}};
        g.createAdjList(a);
-       g.dfs(0); // starting node to dfs is 0
+       g.dfs(0); // starting node to bfs is 0
+       g.bfs(0);  // starting node to bfs is 0
     }
 }
-//Depth-First Search
+//Breadth-First Search
 class Node{
     int label;
     Node next; // this object indicates the next one after it.
     Node(int b){
         label = b;
+    }
+}
+class Queue{
+    private int maxSize; // max queue size
+    private int[] que; // que is an array of integers
+    private int front;
+    private int rear;
+    private int count; // count of items in queue
+    public Queue(int s){// constructor
+      maxSize = s;
+      que = new int[maxSize];
+      front = rear = -1;
+    }
+    public void qinsert(int item){
+        if(rear == maxSize-1)
+        System.out.println("Queue is Full");
+        else{
+            rear = rear + 1 ;
+            que[rear] = item;
+            if( front == -1)
+            front = 0;
+        }
+    }
+    public int qdelete(){
+        int item;
+        if(IsEmpty()){
+            System.out.println("\n  Queue is Empty");
+            return(-1);
+        }
+        item = que[front];
+        if( front == rear)
+        front = rear = -1;
+        else front = front + 1;
+        return(item);
+    }
+    public boolean IsEmpty(){
+        return(front == -1);
     }
 }
 class Graph{
@@ -24,6 +64,7 @@ class Graph{
         mark = new int[size]; // elements of the mark initialized to 0
     }
     public void createAdjList(int a[][]){
+
 
         Node p; int i, k;
         for(i = 0; i<size; i++){
@@ -37,16 +78,35 @@ class Graph{
         }
     }
     public void dfs(int head){// recursive depth-first search
-      Node w;
-      int v;
-      mark[head] = 1;
-      System.out.print(head +" ");
-      w = adjList[head];
-      while( w != null){
-          v = w.label;
-          if( mark[v] == 0)
-          dfs(v);
-          w = w.next;
+        Node w;
+        int v;
+        mark[head] = 1;
+        System.out.print(head +" ");
+        w = adjList[head];
+        while( w != null){
+            v = w.label;
+            if( mark[v] == 0)
+            dfs(v);
+            w = w.next;
+        }
       }
-    } 
-}
+    public void bfs(int head)
+    { int v; Node adj;
+    Queue q = new Queue(size);
+    v = head;
+    mark[v] = 1; System.out.print(v + "");
+    q.qinsert(v);
+    while( !q.IsEmpty() ){ 
+        v = q.qdelete(); 
+        adj = adjList[v];
+          while( adj != null ){ 
+              v = adj.label;
+              if( mark[v] == 0 ){ 
+                  q.qinsert(v);
+                  mark[v] = 1; 
+                  System.out.print(v + "");}
+                  adj = adj.next;
+                }
+            }
+        }
+    } // end of Graph class
